d8b8031bf714da27aa5f69039e1154ceea50e971
Furkan Kurtoglu <fkurtog@iu.edu>
2019-05-01 22:40:00 -0400

    gitignore update

--- PhysiCell_Folder/TemporalPlot.m
+++ PhysiCell_Folder/TemporalPlot.m
@@ -4,7 +4,8 @@ clc
 
 
 
-cd C:\Users\Aneequa\Desktop\Biofilm-Project\PhysiCell_Folder\output
+cd C:\Users\Furkan\Documents\GitHub\Biofilm-Project\PhysiCell_Folder
+cd output
 
 % 
 % cd C:\Users\Furkan\Desktop\output_100_um_tumor_1e-6_degradation
@@ -25,11 +26,12 @@ for i = 1:length(MEMatFiles)
     O2 = multiscale_microenvironment(5,:);
     Glucose = multiscale_microenvironment(7,:);
     ECM = multiscale_microenvironment(6,:);
+%     Autoinducer = multiscale_microenvironment(8,:);
     O2 = reshape(O2, [25 25]);
     Glucose = reshape(Glucose, [25 25]);
-    
-    
+%     Autoinducer = reshape(Autoinducer, [25 25]);
     ECM = reshape(ECM, [25 25]);
+    
     XPos = multiscale_microenvironment(1,:);
     YPos = multiscale_microenvironment(2,:);
     XPos = reshape(XPos, [25 25]);
@@ -52,7 +54,7 @@ for i = 1:length(MEMatFiles)
     axis image;
     
     
-    ax1 = subplot(2,2,3.5);
+    ax1 = subplot(2,2,3);
     contourf(XPos,YPos,ECM,'linecolor','none');
  %   caxis(ax3,[-0.001 1]);
     colorbar('eastoutside');
@@ -60,10 +62,18 @@ for i = 1:length(MEMatFiles)
     title('ECM');
     axis image;    
     
+%     ax4 = subplot(2,2,4);
+%     contourf(XPos,YPos,Autoinducer,'linecolor','none');
+%  %   caxis(ax3,[-0.001 1]);
+%     colorbar('eastoutside');
+%     set(gca,'color',[0.2422,0.1504,0.6603]);
+%     title('Autoinducer');
+%     axis image;    
+    
     
     BigTitle = num2str(i/24);
-%    suptitle(['Time = ',BigTitle,' days']);
-    pause(0.5);
+    suptitle(['Time = ',BigTitle,' days']);
+    pause(0.05);
 end
 
 %%
